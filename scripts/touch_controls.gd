d02ccad893f980c7f2b872d75363becaf437fab3
Mehranj73 <9513152102mMn!>
2024-06-03 06:14:58 +0330

    first commit

---
+++ scripts/touch_controls.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var left = $Left
+@onready var right = $Right
+@onready var jump = $Jump
+
+
+func _on_left_pressed():
+	pass # Replace with function body.
+
+
+func _on_left_released():
+	pass # Replace with function body.
